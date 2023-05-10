93ffe8f1500616d744e45746a45189716f219509
Agfan Herru Pratama <107943090+agfanpratama@users.noreply.github.com>
2023-05-10 21:29:09 +0700

    Membuat Splash Screen di Mobile Programming

---
+++ Prak5/Prak5/app/src/main/java/com/example/prak5/MainAlert.java
@@ -0,0 +1,68 @@
+package com.example.prak5;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+public class MainAlert extends Activity {
+    private Button button1;
+    private Button button2;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_alert);
+        button1 = (Button) findViewById(R.id.one_button_alert);
+        button2 = (Button) findViewById(R.id.two_button_alert);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainAlert.this);
+                builder.setTitle("Contoh Alert");
+                builder.setMessage("Alert dengan 1 Action Button ");
+                builder.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Log.e("info", "OK");
+                            }
+                        });
+                builder.show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainAlert.this);
+                builder.setTitle("Contoh Alert");
+                builder.setMessage("Alert dengan 2 Action Button ");
+                builder.setNegativeButton("NO",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Log.e("info", "NO");
+                            }
+                        });
+                builder.setPositiveButton("YES",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Log.e("info", "YES");
+                            }
+                        });
+                builder.show();
+            }
+        });
+
+    }
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
